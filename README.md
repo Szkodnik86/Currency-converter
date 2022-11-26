91ffbe28e0a9e515f85cf257f0570b1cd03fac33
Tomasz Szkudlarek <Tomasz.szkudlarek25@gmail.com>
2022-11-26 12:26:15 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Tomasz Szkudlarek 
+
+## first page on github
+
+### This is just currency conversion, it's part of the "youcode" frontend course
+https://szkodnik86.github.io/converter/
+
+
+
+and of course a picture of me with my son
+![Myfoto](https://github.com/Szkodnik86/converter/blob/main/images/myphoto.jpg?raw=true)
---
+++ js/script.js
@@ -0,0 +1,35 @@
+{
+    const welcome = () => {
+        console.log("Witam wszystkich odwiedzających DEV", "strona dodana do GIT.");
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const exchangeElement = document.querySelector(".js-converter__exchange");
+        const currencyElement = document.querySelector(".js-converter__currency");
+        const resultElement = document.querySelector(".js-result");
+        const result = calculateResult(currencyElement, exchangeElement);
+
+        updateResultText(result, resultElement);
+    };
+    
+    const calculateResult = (currencyElement, exchangeElement) => {
+        return currencyElement.value * exchangeElement.value;
+    };
+    
+    const updateResultText = (result, resultElement) => {
+        resultElement.innerText = result.toFixed(2);
+    };
+
+    const init = () => {
+        const formElement = document.querySelector(".js-converter");
+
+        formElement.addEventListener("submit", onFormSubmit);
+
+        welcome();
+    };
+
+    init();
+
+}
